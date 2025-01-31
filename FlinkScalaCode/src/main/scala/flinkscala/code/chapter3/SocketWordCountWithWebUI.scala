89122ab8b8c8de7de18ce350e8ca0cf99cb2b1fc
yangkai.04 <yangkai.04@bytedance.com>
2025-01-31 16:59:41 +0800

    feat: standalone, local webui

--- FlinkScalaCode/src/main/scala/flinkscala/code/chapter3/SocketWordCountWithWebUI.scala
+++ FlinkScalaCode/src/main/scala/flinkscala/code/chapter3/SocketWordCountWithWebUI.scala
@@ -1,5 +1,21 @@
 package flinkscala.code.chapter3
 
-class SocketWordCountWithWebUI {
+import org.apache.flink.configuration.{Configuration, RestOptions}
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
+object SocketWordCountWithWebUI {
+  def main(args: Array[String]): Unit = {
+    val conf = new Configuration()
+    conf.set(RestOptions.BIND_PORT, "8083")
+    val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf)
+    import org.apache.flink.streaming.api.scala._
+
+    val ds = env.socketTextStream("localhost", 9999)
+    ds.flatMap(_.split(","))
+      .map((_, 1))
+      .keyBy(_._1)
+      .sum(1)
+      .print()
+    env.execute()
+  }
 }
